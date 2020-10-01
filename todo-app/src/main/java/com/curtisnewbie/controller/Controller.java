a335493d28ab5ebc5515751862fe0079b85f733c
CurtisNewbie <zhuangyongj@gmail.com>
2020-10-01 16:47:58 +0800

    Impl methods to add TodoJobView into ListView

--- todo-app/src/main/java/com/curtisnewbie/controller/Controller.java
+++ todo-app/src/main/java/com/curtisnewbie/controller/Controller.java
@@ -3,7 +3,6 @@ package com.curtisnewbie.controller;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.ListView;
-import javafx.scene.layout.HBox;
 
 import java.net.URL;
 import java.util.ResourceBundle;
@@ -20,15 +19,33 @@ public class Controller implements Initializable {
     @FXML
     private ListView<TodoJobView> listView;
 
+    /**
+     * Add {@code TodoJobView} into the {@code ListView}
+     *
+     * @param jobView
+     */
+    public void addTodoJobView(TodoJobView jobView) {
+        jobView.prefWidthProperty().bind(listView.widthProperty());
+        listView.getItems().add(jobView);
+    }
+
+    /**
+     * Add new {@code TodoJobView} into the {@code ListView}
+     *
+     * @param jobName
+     */
+    public void addTodoJobView(String jobName) {
+        TodoJobView jobView = new TodoJobView(jobName);
+        addTodoJobView(jobView);
+    }
+
     @Override
     public void initialize(URL location, ResourceBundle resources) {
         TodoJobView jobView = new TodoJobView("Feels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels " +
-                "gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels " +
-                "gucci");
-        listView.getItems().add((TodoJobView) jobView);
-        jobView.prefWidthProperty().bind(listView.widthProperty());
-        listView.getItems().add(new TodoJobView("Feels gucci"));
-        listView.getItems().add(new TodoJobView("Feels gucci"));
-        listView.getItems().add(new TodoJobView("Feels gucci"));
+                "gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels gucciFeels " + "gucci");
+        addTodoJobView(jobView);
+        addTodoJobView("Todo job 1");
+        addTodoJobView("Todo job 2");
+        addTodoJobView("Todo job 3");
     }
 }
