96359905a76db8709bb19048fa669d92a626c047
CurtisNewbie <zhuangyongj@gmail.com>
2020-10-01 16:25:51 +0800

    Improve LabelFactory code reusability

--- todo-app/src/main/java/com/curtisnewbie/util/LabelFactory.java
+++ todo-app/src/main/java/com/curtisnewbie/util/LabelFactory.java
@@ -18,16 +18,20 @@ public class LabelFactory {
     }
 
     public static Label getClassicLabel(String name) {
-        Label label = new Label(name);
-        label.setPadding(new Insets(1, 2, 1, 2));
-        label.setBorder(Border.EMPTY);
-        label.setAlignment(Pos.CENTER);
+        Label label = getBaseLabel(name);
+        label.setPadding(new Insets(2, 2, 2, 2));
         return label;
     }
 
     public static Label getLeftPaddedLabel(String name) {
+        Label label = getBaseLabel(name);
+        label.setPadding(new Insets(2, 2, 2, 15));
+        return label;
+    }
+
+    private static Label getBaseLabel(String name) {
         Label label = new Label(name);
-        label.setPadding(new Insets(1, 2, 1, 15));
+        label.setWrapText(true);
         label.setBorder(Border.EMPTY);
         label.setAlignment(Pos.CENTER);
         return label;
