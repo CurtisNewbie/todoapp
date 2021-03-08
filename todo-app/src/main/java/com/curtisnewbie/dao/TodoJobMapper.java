4d997d5861b888bd52b571ebe3f6e8b9dc3aced2
CurtisNewbie <zhuangyongj@gmail.com>
2021-03-08 23:22:11 +0800

    Design TodoJobMapper Api

--- todo-app/src/main/java/com/curtisnewbie/dao/TodoJobMapper.java
+++ todo-app/src/main/java/com/curtisnewbie/dao/TodoJobMapper.java
@@ -1,9 +1,26 @@
 package com.curtisnewbie.dao;
 
+import com.curtisnewbie.entity.TodoJob;
+
+import java.time.LocalDate;
+import java.util.List;
+
 /**
  * @author yongjie.zhuang
  */
 public interface TodoJobMapper {
 
+    List<TodoJob> findById(int id);
 
+    List<TodoJob> findByPage(int page, int limit);
+
+    List<TodoJob> findBetweenDates(LocalDate startDate, LocalDate endDate);
+
+    LocalDate findEarliestDate();
+
+    LocalDate findLatestDate();
+
+    TodoJob updateById(TodoJob todoJob);
+
+    TodoJob deleteById(int id);
 }
--- todo-app/src/main/java/com/curtisnewbie/dao/TodoJobMapperImpl.java
+++ todo-app/src/main/java/com/curtisnewbie/dao/TodoJobMapperImpl.java
@@ -1,6 +1,10 @@
 package com.curtisnewbie.dao;
 
+import com.curtisnewbie.entity.TodoJob;
+
 import java.sql.Connection;
+import java.time.LocalDate;
+import java.util.List;
 
 /**
  * @author yongjie.zhuang
@@ -11,5 +15,40 @@ public final class TodoJobMapperImpl implements TodoJobMapper {
 
     public TodoJobMapperImpl(Connection connection) {
         this.connection = connection;
+    }
+
+    @Override
+    public List<TodoJob> findById(int id) {
+        return null;
+    }
+
+    @Override
+    public List<TodoJob> findByPage(int page, int limit) {
+        return null;
+    }
+
+    @Override
+    public List<TodoJob> findBetweenDates(LocalDate startDate, LocalDate endDate) {
+        return null;
+    }
+
+    @Override
+    public LocalDate findEarliestDate() {
+        return null;
+    }
+
+    @Override
+    public LocalDate findLatestDate() {
+        return null;
+    }
+
+    @Override
+    public TodoJob updateById(TodoJob todoJob) {
+        return null;
+    }
+
+    @Override
+    public TodoJob deleteById(int id) {
+        return null;
     }
 }
