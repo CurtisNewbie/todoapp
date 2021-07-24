650bb4fe5f0670667c800adbfc12cd7855c48489
CurtisNewbie <zhuangyongj@gmail.com>
2021-07-24 17:03:56 +0800

    Add localized text support in TodoJobObjectPrinter

--- todo-app/src/main/java/com/curtisnewbie/io/TodoJobObjectPrinter.java
+++ todo-app/src/main/java/com/curtisnewbie/io/TodoJobObjectPrinter.java
@@ -3,9 +3,9 @@ package com.curtisnewbie.io;
 import com.curtisnewbie.config.Environment;
 import com.curtisnewbie.config.Language;
 import com.curtisnewbie.config.PropertiesLoader;
-import com.curtisnewbie.config.PropertyConstants;
 import com.curtisnewbie.dao.TodoJob;
 
+import static com.curtisnewbie.config.PropertyConstants.*;
 import static com.curtisnewbie.util.DateUtil.toDDmmUUUUSlash;
 
 /**
@@ -28,13 +28,17 @@ public class TodoJobObjectPrinter implements ObjectPrinter<TodoJob> {
 
     @Override
     public String printObject(TodoJob todoJob) {
-        String done = propertiesLoader.getLocalizedProperty(PropertyConstants.TEXT_DONE_KEY);
-        String inProgress = propertiesLoader.getLocalizedProperty(PropertyConstants.TEXT_IN_PROGRESS_KEY);
-        String status = todoJob.isDone() ? done : inProgress;
+        final String doneText = propertiesLoader.getLocalizedProperty(TEXT_DONE_KEY);
+        final String inProgressText = propertiesLoader.getLocalizedProperty(TEXT_IN_PROGRESS_KEY);
+        final String expectedText = propertiesLoader.getLocalizedProperty(TEXT_EXPECTED_END_DATE_KEY);
+        final String actualText = propertiesLoader.getLocalizedProperty(TEXT_ACTUAL_END_DATE_KEY);
+        String status = todoJob.isDone() ? doneText : inProgressText;
         int width = environment.getLanguage().equals(Language.ENG) ? ENG_WIDTH : CN_WIDTH;
-        return String.format("%-" + width + "s Expected: %s - Actual: %s \n\t%s\n",
+        return String.format("%-" + width + "s %s: %s - %s: %s \n\t%s\n",
                 "[" + status + "]",
+                expectedText,
                 toDDmmUUUUSlash(todoJob.getExpectedEndDate()),
+                actualText,
                 todoJob.getActualEndDate() != null ? toDDmmUUUUSlash(todoJob.getActualEndDate()) : "__/__/____",
                 formatContent(todoJob.getName()));
     }
